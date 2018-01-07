3e2f5384db692b80538e4c72e17c91b9e1f158ff
unknown <alyciarileym@gmail.com>
2018-01-06 20:25:39 -0600

    added button styling

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,10 +1,3 @@
-//declare global variables
-
-//elements that will be added to site
-    //dynamic buttons
-    //search bar
-    //gifs (append div?)
-
 //initial array of ghouls
 
 var ghouls = ['Babadook', 'Norman Bates', 'The Boogeyman', 'Chupacabra', 'Donald Trump', 'Cujo', 'Gremlins', 'Jigsaw', 'Freddy Krueger', 'Krampus', 'Hannibal Lecter', 'Jack Torrance', 'Jason Voorhees', 'Annie Wilkes', 'Pennywise', 'Chucky', 'Captain Spaulding'];
@@ -14,18 +7,10 @@ function renderButtons () {
     $("#buttons").empty();
     
     for (var i = 0; i < ghouls.length; i++) {
-   
-        
-          // Then dynamicaly generating buttons for each ghoul in the array.
-          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class
+          var a = $("<button class=button>");
           a.addClass("ghouls");
-          // Adding a data-attribute with a value of the movie at index i
           a.attr("data-ghoul", ghouls[i]);
-          // Providing the button's text with a value of the movie at index i
           a.text(ghouls[i]);
-          // Adding the button to the HTML
           $("#buttons").append(a);
     }
 };
@@ -33,14 +18,12 @@ function renderButtons () {
   
 
 //User interaction starts
-
 //User clicks button
-
-    
 $("#buttons").on("click", function() {
    
     
 //button calls 10 gifs from giphy(static)
+ 
    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + ghouls + "&api_key=hEOJpTZTXZTmdI9Wu2qQpiO2COrWCj5q&limit=10";
     
     $.ajax({
@@ -54,8 +37,7 @@ $("#buttons").on("click", function() {
                 var gifDiv = $("<div class='item'>");
                 var rating = results[j].rating;
                 var p = $("<p>").text("Rating: " + rating);
-                
-                var ghoulImage = $("<img>");
+                var ghoulImage = $("<img id=ghoulGif>");
                 
                 ghoulImage.attr("src", results[j].images.fixed_height.url);
                 gifDiv.append(p);
@@ -64,34 +46,25 @@ $("#buttons").on("click", function() {
             }
         
         console.log(queryURL);
+        console.log(response.data);
     })
-     ghouls = $(this).attr("data-ghoul");
 
 });
 
-//gifs appear in div
+ 
 
-
-
-//User clicks gif and it animates
-
-//user can add own button
 
   // This function handles events where one button is clicked
       $("#add-ghoul").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
         event.preventDefault();
-
-        // This line will grab the text from the input box
+// This line will grab the text from the input box
         var addGhoul = $("#ghoul-input").val().trim();
-        // The movie from the textbox is then added to our array
+        // The ghoul from the textbox is then added to our array
         ghouls.push(addGhoul);
 
-        // calling renderButtons which handles the processing of our movie array
+        // calling renderButtons which handles the processing of our ghoul array
         renderButtons();
       });
 
-      // Calling the renderButtons function at least once to display the initial list of movies
+      // Calling the renderButtons function at least once to display the initial list of ghouls
       renderButtons();
-
